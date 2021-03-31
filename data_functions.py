1335be69dd22d20cd3be2716e31f2c9e232bb210
Agasayan <matfizgiz@gmail.com>
2021-03-31 20:35:46 +0200

    almost final

--- data_functions.py
+++ data_functions.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import json
+
+COLUMNS_TO_NORMALIZE = ['dob', 'daily_commute', 'friends_number', 'relationship_status', 'education',
+                        'location_population']
+
+
 def sex_from_name(dataFrame):
     with open("Additional_Files/polish_male_firstnames.txt") as file:
         male_names = file.read().splitlines()
@@ -9,6 +16,22 @@ def sex_from_name(dataFrame):
                 dataFrame['sex'][i] = 'male'
             elif dataFrame['name'][i] in female_names:
                 dataFrame['sex'][i] = 'female'
+
+
+def get_fitness_related_words():
+    with open('Additional_Files/related_words.txt') as file:
+        set_of_words = set(file.read().splitlines())
+    return set_of_words
+
+
+def check_if_fitness_related(data):
+    if data == 0:
+        return 0
+    words = get_fitness_related_words()
+    for word in data.split():
+        if word in words:
+            return 1
+    return 0
 
 
 def sex_to_binary(dataFrame):
@@ -30,8 +53,63 @@ def dob_preprocessing(dataFrame):
     dataFrame['dob'] = dataFrame['dob'].apply(lambda x: (2021 - int(x[0:4])) if isinstance(x, str) else float('NaN'))
 
 
+def hobbies_preprocessing(dataFrame):
+    dataFrame['hobbies'] = dataFrame['hobbies'].fillna(0)
+    dataFrame['hobbies'] = dataFrame['hobbies'].apply(lambda x: x.replace(',', ' ').lower() if x != 0 else 0)
+    dataFrame['hobbies'] = dataFrame['hobbies'].apply(check_if_fitness_related)
+
+
+def normalize_column(dataFrame, column_name):
+    min_value = dataFrame[column_name].min()
+    max_value = dataFrame[column_name].max()
+
+    def normalize_function(x, min=min_value, max=max_value):
+        return (x - min) / (max - min)
+
+    dataFrame[column_name] = dataFrame[column_name].apply(normalize_function)
+
+
 def data_preprocessing(dataFrame):
     sex_preprocessing(dataFrame)
     relationship_preprocessing(dataFrame)
     dob_preprocessing(dataFrame)
+    hobbies_preprocessing(dataFrame)
 
+
+def data_normalization(dataFrame):
+    for name in COLUMNS_TO_NORMALIZE:
+        normalize_column(dataFrame, name)
+
+
+def one_hot_encoding(n_classes, Y):
+    results = []
+    for i in range(Y.size):
+        tmp = [0] * n_classes
+        tmp[int(Y[i])] = 1
+        results.append(tmp)
+    return pd.DataFrame(results)
+
+
+def get_groups_from_json(name):
+    with open('Case_Assignment/' + name + '.json') as f:
+        related_words = get_fitness_related_words()
+        data = json.load(f)
+        data = data['data']
+        users_groups = []
+        for record in data:
+            id = record['id']
+            groups = [group['group_name'] for group in record['groups']['data']]
+            users_groups.append((int(id), [name.replace(',', ' ').split() for name in groups]))
+        users_groups.sort(key=lambda x: x[0])
+        for i in range(len(users_groups)):
+            for group in users_groups[i][1]:
+                for word in group:
+                    if word in related_words:
+                        users_groups[i] = (users_groups[i][0], 1)
+                        break
+                if users_groups[i][1] == 1:
+                    break
+            if users_groups[i][1] != 1:
+                users_groups[i] = (users_groups[i][0], 0)
+        dataFrame = pd.DataFrame({'groups': [value[1] for value in users_groups]})
+    return dataFrame
--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import pandas as pd
 import numpy as np
 import sys
-from impyute.imputation.cs import mice
-from impyute.imputation.cs import fast_knn
-from data_functions import data_preprocessing
+from impyute.imputation.cs import fast_knn, mice
+from data_functions import data_preprocessing, one_hot_encoding, data_normalization, get_groups_from_json
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 import matplotlib.pyplot as plt
+from keras import layers
+from keras import models
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+COLUMNS = ['target', 'sex', 'dob', 'daily_commute', 'friends_number', 'relationship_status', 'education',
+           'hobbies', 'location_population', 'groups']
 
 
-def train_random_forest(X, Y, estimators):
+def train_random_forest(X, Y, estimators, classes, features):
     train_x = X[0:3600, :]
     train_y = Y[0:3600]
     validate_x = X[3600:, :]
     validate_y = Y[3600:]
     clf = RandomForestClassifier(n_estimators=estimators)
-    clf.n_classes_ = 2
-    clf.n_features_ = 6
+    clf.n_classes_ = classes
+    clf.n_features_ = features
     clf.fit(train_x, train_y)
     predictions = clf.predict(validate_x).reshape(400, 1)
     result = sum([1 if predictions[i] == validate_y[i] else 0 for i in range(validate_y.shape[0])]) / \
@@ -23,39 +30,112 @@ def train_random_forest(X, Y, estimators):
     return result
 
 
-def train_ada_boost(X, Y, estimators):
+def train_ada_boost(X, Y, estimators, classes, features):
     train_x = X[0:3600, :]
     train_y = Y[0:3600]
     validate_x = X[3600:, :]
     validate_y = Y[3600:]
     clf = AdaBoostClassifier(n_estimators=estimators)
-    clf.n_classes_ = 2
-    clf.n_features_ = 6
+    clf.n_classes_ = classes
+    clf.n_features_ = features
     clf.fit(train_x, train_y)
     predictions = clf.predict(validate_x).reshape(400, 1)
     result = sum([1 if predictions[i] == validate_y[i] else 0 for i in range(validate_y.shape[0])]) / \
              validate_y.shape[0] * 100
     return result
+
+
+def train_logistic_regression(X, Y, classes, features):
+    train_x = X[0:3600, :]
+    train_y = Y[0:3600]
+    validate_x = X[3600:, :]
+    validate_y = Y[3600:]
+    clf = LogisticRegression()
+    clf.n_classes_ = classes
+    clf.n_features_ = features
+    clf.fit(train_x, train_y)
+    predictions = clf.predict(validate_x).reshape(400, 1)
+    result = sum([1 if predictions[i] == validate_y[i] else 0 for i in range(validate_y.shape[0])]) / \
+             validate_y.shape[0] * 100
+    return result
+
+
+def train_neural_network(X, Y, epochs, features, classes):
+    X_train = X[:3200, :]
+    Y_train = one_hot_encoding(classes, Y[:3200])
+    X_val = X[3200:3600, :]
+    Y_val = one_hot_encoding(classes, Y[3200:3600])
+    X_test = X[3600:4000, :]
+    Y_test = one_hot_encoding(classes, Y[3600:4000])
+
+    model = models.Sequential()
+
+    model.add(layers.Dense(18, activation='relu', input_shape=(features,)))
+    model.add(layers.Dense(18, activation='relu'))
+    model.add(layers.Dense(9, activation='relu'))
+    model.add(layers.Dense(2, activation='softmax'))
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    history = model.fit(X_train, Y_train, epochs=epochs, batch_size=256, validation_data=(X_val, Y_val))
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    plt.clf()
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    model.evaluate(X_test, Y_test)
+
+    # results = model.predict(X_test)
+    # Y_predictions = [0 if values[0] > values[1] else 1 for values in results]
+    # sns.heatmap(confusion_matrix(Y[3600:4000], Y_predictions), annot=True)
+    # plt.show()
 
 
 sys.setrecursionlimit(100000)
 a = pd.read_csv("Case_Assignment/train.csv")
 a = a.sort_values(['user_id'])
 data_preprocessing(a)
-X = a[['target', 'sex', 'dob', 'daily_commute', 'friends_number', 'relationship_status', 'education']].to_numpy()
+data_normalization(a)
+a = pd.concat([a, get_groups_from_json('train')], axis=1)
+X = a[COLUMNS].to_numpy()
 np.random.shuffle(X)
 X, Y = X[:, 1:], X[:, 0]
-X3 = fast_knn(X, 400).round()
+X2 = mice(X).round()
+X = fast_knn(X, 400).round()
 
+# train_neural_network(X, Y, 20, 9, 2)
 N = 100
-n_models = 10
-estimators = 100
+estimators = 400
 random_forest_results = []
+random_forest_results2 = []
 ada_boost_results = []
+logistic_regression_results = []
 for i in range(N):
-    random_forest_results.append(train_random_forest(X3, Y, estimators))
-    ada_boost_results.append(train_ada_boost(X3, Y, estimators))
+    random_forest_results.append(train_random_forest(X, Y, estimators, 2, 9))
+    random_forest_results2.append(train_random_forest(X2, Y, estimators, 2, 9))
+    # ada_boost_results.append(train_ada_boost(X, Y, estimators, 2, 9))
+    # logistic_regression_results.append(train_logistic_regression(X, Y, 2, 9))
 fig = plt.figure()
-plt.plot(range(N), ada_boost_results, '.-r')
-plt.plot(range(N), random_forest_results, 'o-b')
+plt.plot(range(N), random_forest_results, '.-r', label='KNN data')
+plt.plot(range(N), random_forest_results2, 'o-b', label='Mice data')
+plt.legend()
+# plt.plot(range(N), random_forest_results, 'o-b')
+# plt.plot(range(N), logistic_regression_results, '+-y')
 plt.show()
