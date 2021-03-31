794103510cad28eefce2676d1535ce8d8522ab53
Agasayan <matfizgiz@gmail.com>
2021-03-31 22:36:35 +0200

    almost almost final

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import sys
-from impyute.imputation.cs import fast_knn, mice
+from impyute.imputation.cs import mice
 from data_functions import data_preprocessing, one_hot_encoding, data_normalization, get_groups_from_json
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 import matplotlib.pyplot as plt
@@ -60,21 +60,23 @@ def train_logistic_regression(X, Y, classes, features):
     return result
 
 
-def train_neural_network(X, Y, epochs, features, classes):
+def train_neural_network(X, Y, epochs, features, classes, X_test):
     X_train = X[:3200, :]
     Y_train = one_hot_encoding(classes, Y[:3200])
     X_val = X[3200:3600, :]
     Y_val = one_hot_encoding(classes, Y[3200:3600])
     X_test = X[3600:4000, :]
     Y_test = one_hot_encoding(classes, Y[3600:4000])
-
+    # Y = one_hot_encoding(2, Y)
     model = models.Sequential()
 
-    model.add(layers.Dense(18, activation='relu', input_shape=(features,)))
-    model.add(layers.Dense(18, activation='relu'))
-    model.add(layers.Dense(9, activation='relu'))
+    model.add(layers.Dense(16, activation='relu', input_shape=(features,)))
+    model.add(layers.Dense(16, activation='relu'))
+    model.add(layers.Dense(8, activation='relu'))
     model.add(layers.Dense(2, activation='softmax'))
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    # history = model.fit(X, Y, epochs=epochs, batch_size=256)
+    # predictions = model.predict(X_test)
     history = model.fit(X_train, Y_train, epochs=epochs, batch_size=256, validation_data=(X_val, Y_val))
 
     loss = history.history['loss']
@@ -109,33 +111,40 @@ def train_neural_network(X, Y, epochs, features, classes):
 
 
 sys.setrecursionlimit(100000)
-a = pd.read_csv("Case_Assignment/train.csv")
-a = a.sort_values(['user_id'])
-data_preprocessing(a)
-data_normalization(a)
-a = pd.concat([a, get_groups_from_json('train')], axis=1)
-X = a[COLUMNS].to_numpy()
+X_train = pd.read_csv("Case_Assignment/train.csv")
+X_train = X_train.sort_values(['user_id'])
+data_preprocessing(X_train)
+data_normalization(X_train)
+X_train = pd.concat([X_train, get_groups_from_json('train')], axis=1)
+X = X_train[COLUMNS].to_numpy()
 np.random.shuffle(X)
 X, Y = X[:, 1:], X[:, 0]
-X2 = mice(X).round()
-X = fast_knn(X, 400).round()
 
-# train_neural_network(X, Y, 20, 9, 2)
+X = mice(X)
+
+# X_test = pd.read_csv("Case_Assignment/test.csv")
+# X_test = X_test.sort_values((['user_id']))
+# data_preprocessing(X_test)
+# data_normalization(X_test)
+# X_test = pd.concat([X_test, get_groups_from_json('test')], axis=1)
+# X_test = X_test[COLUMNS].to_numpy()
+# X_test = X_test[:, 1:]
+# X_test = mice(X_test)
+
+# train_neural_network(X, Y, 25, 9, 2, None)
 N = 100
-estimators = 400
+estimators = 200
 random_forest_results = []
 random_forest_results2 = []
 ada_boost_results = []
 logistic_regression_results = []
 for i in range(N):
     random_forest_results.append(train_random_forest(X, Y, estimators, 2, 9))
-    random_forest_results2.append(train_random_forest(X2, Y, estimators, 2, 9))
-    # ada_boost_results.append(train_ada_boost(X, Y, estimators, 2, 9))
-    # logistic_regression_results.append(train_logistic_regression(X, Y, 2, 9))
+    ada_boost_results.append(train_ada_boost(X, Y, estimators, 2, 9))
+    logistic_regression_results.append(train_logistic_regression(X, Y, 2, 9))
 fig = plt.figure()
-plt.plot(range(N), random_forest_results, '.-r', label='KNN data')
-plt.plot(range(N), random_forest_results2, 'o-b', label='Mice data')
+plt.plot(range(N), random_forest_results, '.-r', label='random_forest')
+plt.plot(range(N), ada_boost_results, 'o-b', label='ada_boost')
+plt.plot(range(N), logistic_regression_results, '+-y', label='logistic_regression')
 plt.legend()
-# plt.plot(range(N), random_forest_results, 'o-b')
-# plt.plot(range(N), logistic_regression_results, '+-y')
 plt.show()
